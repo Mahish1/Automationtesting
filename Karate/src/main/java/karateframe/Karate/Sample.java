5b920dc4056247f50577afee2d4fd9295bd0f0c6
Lenovo <Lenovo@DESKTOP-RMLRT29>
2025-09-17 16:56:34 +0530

    Test

---
+++ Karate/src/main/java/karateframe/Karate/Sample.java
@@ -0,0 +1,22 @@
+package karateframe.Karate;
+
+import com.intuit.karate.driver.chrome.ChromeWebDriver;
+
+public class Sample {
+
+	public static void main(String[] args) {
+		
+	
+		// TODO Auto-generated method stub
+		
+String name="Mahesh";
+String na="";
+for (char it  : name.toCharArray()) {
+	
+	na=it+na;
+	
+}
+System.out.println(na);
+
+	}
+}
